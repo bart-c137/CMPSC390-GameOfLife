9cf837564eb023dcc6ecb745c80e443d361f566c
bart-c137 <48188008+bart-c137@users.noreply.github.com>
2020-03-12 21:01:53 -0500

    Changed welcome message.

--- src/com/sxu/bart/Program.java
+++ src/com/sxu/bart/Program.java
@@ -2,6 +2,6 @@ package com.sxu.bart;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome to the Game of Life");
     }
 }
